d8b3b3764929d86a1c27402dc96fefc6e3d76337
NeilTohno <neil_tohno@live.com>
2021-03-05 18:47:56 +0800

    Update _gen2.bat

--- _gen2.bat
+++ _gen2.bat
@@ -1,4 +1,4 @@
-@echo on
+@echo off
 mkdir "%~dp1move\"
 ::mkdir "%~dp1RAR\"
 
@@ -16,25 +16,22 @@ echo Start %LIST% %time%
 echo ****************************************************************
 echo.
 call "_generate1.bat" "%LIST%"
-::ping localhost -n 6 >nul
+
 echo.
 echo.
 echo ****************************************************************
 echo End %LIST% %time%
-::ping localhost -n 6 >nul
+
 echo.
 
 ::mkdir "%~dp1move\"
 move "%LIST%" "%~dp1move\"
-::ping localhost -n 6 >nul
+
 
 set LIST=
 for %%f in (.\or\*) do set LIST=%LIST%%%f
-::ping localhost -n 6 >nul
 
 echo.
-rem shift
-rem goto 720
 
 
 :stock
@@ -42,12 +39,9 @@ rem goto 720
 set LIST=
 for %%f in (.\or\*) do set LIST=%LIST%%%f
 
-::move "%LIST%" "%~dp1"
-::ping localhost -n 6 >nul
 
 if exist "%LIST%" (goto generateh) else (goto end)
 
- 
 
 :end 
 echo ****************************************************************
